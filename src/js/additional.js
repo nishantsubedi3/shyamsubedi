253222edf68c986fe5d4254633f19c7fda243eb2
Nishant Subedi <87087828+jojoo122@users.noreply.github.com>
2023-05-22 19:11:51 +0545

    up

---
+++ src/js/additional.js
@@ -0,0 +1,10 @@
+function sorry()
+    {
+       var name = document.getElementById("name").value;
+       alert("Dear, " +name+ " the form is currently unavailable!");
+    }
+
+    function soon()
+    {
+      alert("Dear, user the feature is comming soon.");
+    }
